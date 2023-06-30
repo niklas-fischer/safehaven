57af6ce4a9ad7cd97fa9da0b65e4bab27001430b
Niklas Fischer <niklas.fischer+git@mailbox.org>
2023-06-30 09:58:40 +0200

    `bet.py`: Minor error corrections

--- modules/bet.py
+++ modules/bet.py
@@ -1,8 +1,9 @@
 import numpy as np
+from scipy.stats.mstats import gmean
 
 class Bet:
     """
-    This class represents a bet, with a name, results, weights, and an outcome.
+    This class represents a bet, with a name, results, weights, and calulations for outcome, arithmetic mean, and geometric mean.
     """
 
     def __init__(self, name, results, weights):
@@ -11,9 +12,11 @@ class Bet:
 
         Parameters:
         name (str): The name of the bet.
-        results (list): A list with 1 to 6 elements representing results.
-        weights (list): A list with equal number of elements as results representing the weights. Each element must be an integer greater or equal to 1.
+        results (array): An array with 1 to 6 elements representing results of a dice roll
+        weights (array): An array representing how often each result occurs. Each element must be an integer greater or equal to 1.
 
+
+        
         Raises:
         AssertionError: If results and weights do not have the same length, or if results has not 1 to 6 elements, or if any element in weights is not an integer greater or equal to 1.
         """
@@ -24,20 +27,24 @@ class Bet:
         # Ensure that results has 1 to 6 elements
         assert len(results) >= 1 and len(results) <= 6, "Results must have 1 to 6 elements."
 
+        # Ensure name is a string
+        assert isinstance(name, str), "Name must be a string."
+
         # Ensure that all elements in weights are integers and are greater or equal to 1
         assert all(isinstance(i, int) and i >= 1 for i in weights), "All elements in weights must be integers greater or equal to 1."
 
-        self.name = name
-        self.results = np.array(results)
-        self.weights = np.array(weights)
-
-        # Calculate the outcome upon initialization
+        self.name = name.lower()
+        self.results = np.array(results) 
+        self.weights = np.array(weights) 
+        
+        # Calculation upon initialization
         self.outcomes = self.calculate_outcomes()
+        self.arith_mean = self.calculate_arith_mean()
+        self.geom_mean = self.calculate_geom_mean()
 
     def calculate_outcomes(self):
         """
-        Calculate the outcome by repeating each element in results as many times as the corresponding weight.
-        The outcome is stored as a numpy array.
+        Iterates through the results and weights to give each dice site an outcome.
 
         Returns:
         np.array: The outcome as a numpy array.
@@ -51,3 +58,82 @@ class Bet:
             # Add the repeated values to the outcome
             outcomes = np.concatenate((outcomes, repeated_values))
         return outcomes
+
+    def calculate_arith_mean(self):
+        """
+        Calculate the arithmetic mean of the outcome.
+        
+        Returns:
+        float: The arithmetic mean of the outcome.
+        """
+        arith_mean = (np.mean(self.outcomes) - 1) * 100
+        return arith_mean
+    
+    def calculate_geom_mean(self):
+        """
+        Calculate the geometric mean of the outcome.
+        
+        Returns:
+        float: The geometric mean of the outcome.
+        """
+        geom_mean = (gmean(self.outcomes) - 1) * 100
+        return geom_mean
+    
+class BetComparison:
+    """
+    This class represents a comparison between two Bet objects.
+    """
+
+    def __init__(self, bet1, bet2, ratio):
+        """
+        Constructor of the BetComparison class.
+
+        Parameters:
+        bet1 (Bet): The first bet to be compared.
+        bet2 (Bet): The second bet to be compared.
+        ratio (list): A list with exactly two elements that sum up to 1, representing the ratio of bet1 to bet2.
+        
+        Raises:
+        AssertionError: If ratio does not have exactly two elements or does not sum up to 1.
+        """
+
+        # Ensure that bet1 and bet2 are both instances of the Bet class
+        assert isinstance(bet1, Bet), "bet1 must be an instance of the Bet class."
+        assert isinstance(bet2, Bet), "bet2 must be an instance of the Bet class."
+
+        # Ensure that ratio has exactly two elements and sums up to 1
+        assert len(ratio) == 2, "Ratio must have exactly two elements."
+        assert np.isclose(sum(ratio), 1), "The elements of ratio must sum up to 1."
+
+        self.bet1_name = bet1.name
+        self.bet2_name = bet2.name
+        self.__dict__[self.bet1_name] = bet1
+        self.__dict__[self.bet2_name] = bet2
+        self.ratio = ratio
+        self.arith_mean_combined, self.geom_mean_combined, self.cost, self.net = self.weighted_average()
+
+    def weighted_average(self):
+        """
+        Calculate arithmetic mean of a combined wager of dice and cash.
+        
+        Args:
+        dice_outcomes (array-like): Dice outcomes
+        cash_outcomes (array-like): Cash outcomes
+        dice_to_cash_ratio (array-like): Ratio of dice to cash
+        
+        Returns:
+        dict: Dictionary containing the calculated arithmetic means
+        """
+        
+        # Combine the outcomes based on the given ratio and calculate the arithmetic mean
+        combined_outcomes = self.ratio[0] * self.__dict__[self.bet1_name].outcomes + self.ratio[1] * self.__dict__[self.bet2_name].outcomes
+        
+        # Calculate the arithmetic/geometric mean of the combined outcomes
+        arith_mean_combined = (np.mean(combined_outcomes) - 1) * 100
+        geom_mean_combined = (gmean(combined_outcomes) - 1) * 100
+        
+        # Calculate the cost
+        cost = arith_mean_combined - self.__dict__[self.bet1_name].arith_mean - self.__dict__[self.bet2_name].arith_mean
+        net = geom_mean_combined - self.__dict__[self.bet1_name].geom_mean - self.__dict__[self.bet2_name].geom_mean
+        
+        return arith_mean_combined, geom_mean_combined, cost, net
