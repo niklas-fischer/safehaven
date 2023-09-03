88bb748110ec2806bf0e672ebb4becd55df8f5da
Niklas Fischer <niklas.fischer+git@mailbox.org>
2023-09-03 18:43:45 +0200

    `xo.py`: added subplot 4 first version (WIP)

--- modules/xo.py
+++ modules/xo.py
@@ -48,6 +48,7 @@ def _plot_return_ranges(ax, sp500, marker):
     Parameters:
     ax: The axis (subplot) on which the function should draw
     sp500 (DataFrame): The input DataFrame containing 'TotalReturn' and 'ReturnRange' columns (data of S&P 500)
+    marker: Marker symbol
     """
 
     # Grouping and Aggregating by `ReturnRange`
@@ -95,7 +96,7 @@ def _plot_safe_haven(ax, safe_haven, categories, marker):
         ax.set_ylim(0, 0.25)
 
     # Seaborn Lineplot
-    sns.lineplot(x=categories, y=safe_haven["outcomes"], marker=marker, linewidth=1, markersize=8, color='#3a89bf', ax=ax)
+    sns.lineplot(x=categories, y=safe_haven["outcomes"], marker=marker, linewidth=1, markersize=8, color='black', ax=ax)
 
     # Additional plot adjustments
     ax.set_xticks([])  # Remove x-axis ticks
@@ -103,6 +104,57 @@ def _plot_safe_haven(ax, safe_haven, categories, marker):
     ax.set_ylabel(safe_haven["title"])
     ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0%}'.format(x)))  # Format y-axis as percentage
 
+
+def _plot_combined_outcome(ax, sp500, safe_haven, weights):
+    """
+    Plots a line chart of a weighted outcome of the S&P 500 and a safe haven prototype.
+
+    Parameters:
+    ax: Plotting axis
+    sp500 (DataFrame): The input DataFrame containing 'TotalReturn' and 'ReturnRange' columns (data of S&P 500)
+    safe_haven (dict): Contains outcomes as an array and a title
+    weights (list): List of two weights for S&P 500 and safe haven respectively
+
+    Returns:
+    None (displays the plot)
+    """    
+    # Grouping and Aggregating by `ReturnRange`, getting min and max data points:
+    grouped = sp500.groupby('ReturnRange')['TotalReturn'].agg(['min', 'max', 'mean'])
+
+    grouped = grouped.sort_values(by='mean')
+
+    bars = ax.bar(grouped.index, grouped['max'] - grouped['min'], bottom=grouped['min'], color='#3a89bf', label="Return Range")
+
+    # Ensure the safe_haven outcomes and the DataFrame have the same length
+    if len(grouped) != len(safe_haven['outcomes']):
+        raise ValueError("Length of safe_haven outcomes does not match DataFrame group length.")
+
+    # Calculate the weighted average
+    weighted_average = (grouped['mean'] * weights[0] + safe_haven['outcomes'] * weights[1]) / sum(weights)
+
+    # Set Y-axis limits
+    ax.set_ylim(-0.5, 1.0)  # -50% to 100%
+
+    # Remove X-axis labels
+    ax.set_xticks([])
+
+    # Add a black line at 0%
+    ax.axhline(0, color='black', linewidth=0.8)
+
+    # Format Y-axis labels as percentages
+    def percent_formatter(x, pos):
+        return f"{x:.0%}"
+    
+    ax.yaxis.set_major_formatter(FuncFormatter(percent_formatter))
+
+    # Plot the weighted average returns with dashed black line
+    ax.plot(grouped.index, weighted_average, color='black', linestyle='--')
+
+    # Mark average returns with 'x' in blue
+    ax.scatter(grouped.index, weighted_average, color='black', marker='^', s=40)
+    
+    # Set plot label
+    ax.set_ylabel(f"{weights[0] * 100} % SPX &\n {weights[1] * 100} % {safe_haven['title']}")
 
 ############# 
 # FUNCTIONS #
@@ -139,14 +191,14 @@ def plot_xo_sp500(sp500, safe_haven, weights):
     # Subplot 2: S&P 500 return ranges
     _plot_return_ranges(ax2, sp500, 'x')
 
-    # Subplot 3: Height of bet2 outcome
+    # Subplot 3: Plot safe haven cartoon outcome
     _plot_safe_haven(ax3, safe_haven, categories, 'o')
 
-    # Subplot 4: Height of combined outcome
-    #_plot_combined_outcome(ax4, bet_comparison)
+    # Subplot 4: Combined safe haven cartoon prototype outcome
+    _plot_combined_outcome(ax4, sp500, safe_haven, weights)
 
     # Adjust spacing between subplots
     plt.tight_layout()
 
     # Saving plot
-    plt.savefig('../plots/xo_sp500.png', dpi=300)
+    plt.savefig(f"../plots/xo_sp500_{safe_haven['title']}.png", dpi=300)
